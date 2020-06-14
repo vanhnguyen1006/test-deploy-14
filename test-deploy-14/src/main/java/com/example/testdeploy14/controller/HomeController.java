9e9860994354517501ccaa7e2452216fe792e125
Vanh Nguyen <57629118+vanhnguyen1006@users.noreply.github.com>
2020-06-14 20:47:42 +0700

    update 14-6-20 20h56

--- test-deploy-14/src/main/java/com/example/testdeploy14/controller/HomeController.java
+++ test-deploy-14/src/main/java/com/example/testdeploy14/controller/HomeController.java
@@ -8,15 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.support.PagedListHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
+@RequestMapping("/")
+@ResponseBody
 public class HomeController {
     @Autowired
     private final ProductService productService;
