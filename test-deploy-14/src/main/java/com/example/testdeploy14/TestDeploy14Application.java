9b85ac4aa33e267a9bf944532e3ddf502c5db45e
Vanh Nguyen <57629118+vanhnguyen1006@users.noreply.github.com>
2020-06-14 15:38:03 +0700

    First commit

---
+++ test-deploy-14/src/main/java/com/example/testdeploy14/TestDeploy14Application.java
@@ -0,0 +1,13 @@
+package com.example.testdeploy14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDeploy14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDeploy14Application.class, args);
+	}
+
+}
---
+++ test-deploy-14/src/test/java/com/example/testdeploy14/TestDeploy14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testdeploy14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDeploy14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
