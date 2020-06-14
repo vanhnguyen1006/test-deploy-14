abb4e364052f03006b64ff7a1b3aa2bac62ac9b8
Vanh Nguyen <57629118+vanhnguyen1006@users.noreply.github.com>
2020-06-14 17:49:07 +0700

    update 14-6-20 17h57

--- test-deploy-14/src/test/java/com/example/testdeploy14/TestDeploy14ApplicationTests.java
+++ test-deploy-14/src/test/java/com/example/testdeploy14/TestDeploy14ApplicationTests.java
@@ -1,7 +1,7 @@
 package com.example.testdeploy14;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class TestDeploy14ApplicationTests {
